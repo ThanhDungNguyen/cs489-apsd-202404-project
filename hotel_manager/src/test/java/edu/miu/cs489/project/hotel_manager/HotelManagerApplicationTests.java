ccfe0440bf623eb1a5f62a8e34b8f29be8e4b381
Donny Nguyen <donny.nguyen.swe@outlook.com>
2024-04-20 09:02:47 -0500

    Initialize project

---
+++ hotel_manager/src/test/java/edu/miu/cs489/project/hotel_manager/HotelManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.miu.cs489.project.hotel_manager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HotelManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
