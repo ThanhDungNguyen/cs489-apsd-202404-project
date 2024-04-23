d6c63ac2c8c79d40664c877901b2dff1f6b4735a
Thanh Dung Nguyen <thanhdung.nguyen@miu.edu>
2024-04-23 11:51:01 -0500

    Add new project

---
+++ tasks_manager/src/main/java/edu/miu/cs489/project/thanhdungnguyen/tasks_manager/TasksManagerApplication.java
@@ -0,0 +1,13 @@
+package edu.miu.cs489.project.thanhdungnguyen.tasks_manager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TasksManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TasksManagerApplication.class, args);
+	}
+
+}
---
+++ tasks_manager/src/test/java/edu/miu/cs489/project/thanhdungnguyen/tasks_manager/TasksManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.miu.cs489.project.thanhdungnguyen.tasks_manager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TasksManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
