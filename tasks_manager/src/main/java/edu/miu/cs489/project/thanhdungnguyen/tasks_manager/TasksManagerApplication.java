37009885cf6112b45b0750b3a24482bf43b48a4b
Thanh Dung Nguyen <thanhdung.nguyen@miu.edu>
2024-04-25 22:23:14 -0500

    Add admin user

--- tasks_manager/src/main/java/edu/miu/cs489/project/thanhdungnguyen/tasks_manager/TasksManagerApplication.java
+++ tasks_manager/src/main/java/edu/miu/cs489/project/thanhdungnguyen/tasks_manager/TasksManagerApplication.java
@@ -1,13 +1,45 @@
 package edu.miu.cs489.project.thanhdungnguyen.tasks_manager;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import edu.miu.cs489.project.thanhdungnguyen.tasks_manager.model.Employee;
+import edu.miu.cs489.project.thanhdungnguyen.tasks_manager.model.Role;
+import edu.miu.cs489.project.thanhdungnguyen.tasks_manager.repository.EmployeeRepository;
+import edu.miu.cs489.project.thanhdungnguyen.tasks_manager.repository.RoleRepository;
+import jakarta.annotation.PostConstruct;
 
 @SpringBootApplication
 public class TasksManagerApplication {
+	@Autowired
+	private EmployeeRepository employeeRepository;
+	@Autowired
+	private RoleRepository roleRepository;
+	@Autowired
+	private PasswordEncoder passwordEncoder;
 
 	public static void main(String[] args) {
 		SpringApplication.run(TasksManagerApplication.class, args);
+	}
+
+	@PostConstruct
+	public void initAdminUser() {
+		var adminUser = employeeRepository.findByUsername("admin");
+		if (!adminUser.isPresent()) {
+			List<Role> listAdminRoles = new ArrayList<>();
+			var adminRole = roleRepository.findByName("admin").orElse(new Role(null, "admin", null));
+			listAdminRoles.add(adminRole);
+
+			var newAdminUser = new Employee(null, "Admin", "Admin", "Admin", "admin",
+					passwordEncoder.encode("test1234"), null, null,
+					listAdminRoles, true, true, true, true);
+			employeeRepository.save(newAdminUser);
+		}
 	}
 
 }
--- tasks_manager/src/main/java/edu/miu/cs489/project/thanhdungnguyen/tasks_manager/repository/RoleRepository.java
+++ tasks_manager/src/main/java/edu/miu/cs489/project/thanhdungnguyen/tasks_manager/repository/RoleRepository.java
@@ -1,9 +1,11 @@
 package edu.miu.cs489.project.thanhdungnguyen.tasks_manager.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import edu.miu.cs489.project.thanhdungnguyen.tasks_manager.model.Role;
 
 public interface RoleRepository extends JpaRepository<Role, Integer> {
-
+    Optional<Role> findByName(String name);
 }
