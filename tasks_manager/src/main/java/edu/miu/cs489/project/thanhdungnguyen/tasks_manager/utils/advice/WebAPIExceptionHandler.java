27b315bb124964e9c9bdf9ea5ca043890e990f37
Thanh Dung Nguyen <thanhdung.nguyen@miu.edu>
2024-04-25 22:17:21 -0500

    Implement exception handler advice

---
+++ tasks_manager/src/main/java/edu/miu/cs489/project/thanhdungnguyen/tasks_manager/utils/advice/WebAPIExceptionHandler.java
@@ -0,0 +1,48 @@
+package edu.miu.cs489.project.thanhdungnguyen.tasks_manager.utils.advice;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import edu.miu.cs489.project.thanhdungnguyen.tasks_manager.exception.NoDataException;
+
+@RestControllerAdvice
+public class WebAPIExceptionHandler {
+    @ExceptionHandler(NoDataException.class)
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public Map<String, String> handlePublisherNotFoundException(NoDataException noDataException) {
+        Map<String, String> errorMessageMap = new HashMap<>();
+        errorMessageMap.put("errorMessage", noDataException.getMessage());
+        return errorMessageMap;
+    }
+
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public Map<String, String> handleDataValidationErrors(
+            MethodArgumentNotValidException methodArgumentNotValidException) {
+        var errorMap = new HashMap<String, String>();
+        methodArgumentNotValidException.getBindingResult()
+                .getFieldErrors()
+                .forEach(
+                        error -> errorMap.put(
+                                error.getField(),
+                                error.getDefaultMessage()));
+        return errorMap;
+    }
+
+    // BadCredentialsException
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    @ExceptionHandler(BadCredentialsException.class)
+    public Map<String, String> handleUserAuthBadCredentialException(BadCredentialsException bcEx) {
+        Map<String, String> errorMap = new HashMap<>();
+        errorMap.put("errorMessage", bcEx.getMessage());
+        errorMap.put("errorDisplayText", "Invalid Username and/or Password!");
+        return errorMap;
+    }
+}
