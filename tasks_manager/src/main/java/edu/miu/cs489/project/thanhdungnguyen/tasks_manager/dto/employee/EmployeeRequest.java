de9f7c4579e8d3489cb4232513c910b4e30f11ba
Thanh Dung Nguyen <thanhdung.nguyen@miu.edu>
2024-04-26 10:32:58 -0500

    Set username and password for new employee

--- tasks_manager/src/main/java/edu/miu/cs489/project/thanhdungnguyen/tasks_manager/dto/employee/EmployeeRequest.java
+++ tasks_manager/src/main/java/edu/miu/cs489/project/thanhdungnguyen/tasks_manager/dto/employee/EmployeeRequest.java
@@ -3,6 +3,8 @@ package edu.miu.cs489.project.thanhdungnguyen.tasks_manager.dto.employee;
 public record EmployeeRequest(String firstName,
         String lastName,
         String position,
+        String username,
+        String password,
         Long managerId) {
 
 }
